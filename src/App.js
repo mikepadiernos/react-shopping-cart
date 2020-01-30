dd9c57967324a055fe04ef63c5733127858f33d0
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-29 22:26:33 -0500

    FEATURE:(Cart): added CartContext.js
    - created context for Cart pass properties in ShoppingCart.js & Navigation.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ProductContext from './contexts/ProductContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import CartContext from "./contexts/CartContext";
 
 function App() {
 	const [products] = useState(data);
@@ -19,17 +20,15 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
-
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart} />
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
